993e1373dfb422e0e36f27e29d05e177920251b4
John Marín <jmarinsocha@hotmail.com>
2024-06-02 16:44:10 -0500

    Spring Data, migraciones SQL, creación y configuración del metodo GET.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,16 +1,31 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.DatosListadoMedico;
 import med.voll.api.medico.DatosRegistroMedico;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
     @PostMapping
-    public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico){
-        System.out.println(datosRegistroMedico);
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
+        medicoRepository.save( new Medico(datosRegistroMedico));
+    }
+
+    @GetMapping
+    public Page<DatosListadoMedico> listaMedicos(@PageableDefault(size = 2) Pageable paginacion){
+        return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
     }
 }
--- src/main/java/med/voll/api/direccion/DatosDireccion.java
+++ src/main/java/med/voll/api/direccion/DatosDireccion.java
@@ -1,8 +1,16 @@
 package med.voll.api.direccion;
 
-public record DatosDireccion(String calle,
+import jakarta.validation.constraints.NotBlank;
+
+public record DatosDireccion(
+                             @NotBlank
+                             String calle,
+                             @NotBlank
                              String distrito,
+                             @NotBlank
                              String ciudad,
+                             @NotBlank
                              int numero,
+                             @NotBlank
                              String complemento) {
 }
